2096e32356d4645cc916a35d97618181c4896fae
Tiny-b <kiefkostello@gmail.com>
2025-11-18 09:59:18 +0000

    Add interactive background

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import NavBar from './components/NavBar';
 import MainSection from './components/MainSection';
+import LiquidEther from './components/bg_components/LiquidEther';
+import ProjectGallery from './components/ProjectGallery';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<MainSection />
+			<ProjectGallery />
+			<div
+				style={{
+					width: '100%',
+					height: '1500px',
+					position: 'absolute',
+					zIndex: '-1',
+				}}
+			>
+				<LiquidEther
+					colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+					mouseForce={20}
+					cursorSize={50}
+					isViscous={true}
+					viscous={30}
+					iterationsViscous={32}
+					iterationsPoisson={32}
+					resolution={0.5}
+					isBounce={false}
+					autoDemo={true}
+					autoSpeed={0.5}
+					autoIntensity={2.2}
+					takeoverDuration={0.25}
+					autoResumeDelay={3000}
+					autoRampDuration={0.6}
+				/>
+			</div>
 		</>
 	);
 }
---
+++ src/components/ProjectGallery.jsx
@@ -0,0 +1,25 @@
+import TiltedCard from './card_components/TiltedCard';
+
+function ProjectGallery() {
+	return (
+		<TiltedCard
+			imageSrc='https://i.scdn.co/image/ab67616d0000b273d9985092cd88bffd97653b58'
+			altText='Kendrick Lamar - GNX Album Cover'
+			captionText='Kendrick Lamar - GNX'
+			containerHeight='300px'
+			containerWidth='300px'
+			imageHeight='300px'
+			imageWidth='300px'
+			rotateAmplitude={12}
+			scaleOnHover={1.2}
+			showMobileWarning={false}
+			showTooltip={true}
+			displayOverlayContent={true}
+			overlayContent={
+				<p className='tilted-card-demo-text'>Kendrick Lamar - GNX</p>
+			}
+		/>
+	);
+}
+
+export default ProjectGallery;
